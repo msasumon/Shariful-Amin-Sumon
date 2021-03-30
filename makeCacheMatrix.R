1c478ab0417896a873b83e831f4080b1ba2788f5
msasumon <81503782+msasumon@users.noreply.github.com>
2021-03-30 13:07:14 +0600

    Add files via upload

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,31 +1,23 @@
-setwd('C:/Users/supta/Documents')
-##
-## I simply set the input x as a matrix
-## and then set the solved value "s" as a null
-## then I changed every reference to "mean" to "solve"
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
-  s <- NULL
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
   set <- function(y) {
     x <<- y
-    s <<- NULL
+    inv <<- NULL
   }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  get <- function() {x}
+  setInverse <- function(Inverse) {inv <<- Inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
 }
-##
-## Changed "mean" to "solve" and "m" to "s"
+
 cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
+  inv <- x$getsolve()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+    mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInv(inv)
+  inv
 }
